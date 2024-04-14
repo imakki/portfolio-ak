df96198e86717274ae3eb9c909e22c1c01762539
imakki <ankit.escalate360@gmail.com>
2024-04-15 00:44:56 +0530

    update profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Ankit yadav',
+    // description:
+    //   'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://aneken.vercel.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@kyaankit',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
